fe0e7e195bf7246a0cd76ecf030fa5fb085f9a44
Tambria Kemp <info@cre8visions.com>
2018-11-20 22:42:44 -0500

    clear search from added to js

--- js/index.js
+++ js/index.js
@@ -4,13 +4,11 @@ const GITHUB_API_BASE_URL = 'https://api.github.com';
 
 //function to watch submission of form
 function watchForm () {
-  console.log("im here");
-  
   $('#search').on('submit', function(ev) {
-    ev.preventDefault();
 
+    ev.preventDefault();
     var userInput = $('input').val();
-    
+
     fetchGithubUserRepo(userInput);
     
   })
@@ -22,23 +20,20 @@ function fetchGithubUserRepo(userInput) {
   
     fetch(`${GITHUB_API_BASE_URL}/users/` + userInput + `/repos`)
       .then(res => {
-        // console.log(res)
         return res.json()
       })
-    // .then(json => console.log(json))
     .then(json => displayResults(json))
   
 }
 
 // //function to render results
 function displayResults(json) {
-  console.log('display');
+   $('.results').html('');
   
   for (let i = 0; i < json.length; i++) {  
     $('.results').append(`<li> ${json[i].name} <a href="${json[i].url}">${json[i].name}</a></li>`)
   
   }
-
 }
 
 
